849c9b413a5f548137b30936715c37053d55884a
micaelaedith <romeromicaelaedith@gmail.com>
2025-11-05 18:44:35 -0300

    paso hojas libreta, script e imagenes

--- scripts/btn-libreta.gd
+++ scripts/btn-libreta.gd
@@ -1,10 +1,51 @@
-extends Node
-func _ready():
-	connect("pressed", Callable(self, "_on_pressed"))
+extends TextureRect
 
-func _on_pressed():
-	SoundManager.reproducir_efecto(4)
-	if self.text == "  >  ":
-		print("pagina_derecha")
-	else:
-		print("pagina_izquierda")
+var hojas: Array = []
+var indice_actual: int = 0
+
+@onready var hoja1 = $hoja1
+@onready var hoja2 = $hoja2
+@onready var hoja3 = $hoja3
+@onready var hoja4 = $hoja4
+
+@onready var btn_izquierda = $btn_izquierda
+@onready var btn_derecha = $btn_derecha
+
+func _ready():
+	hojas = []
+	hojas.append(hoja1)
+	hojas.append(hoja2)
+	hojas.append(hoja3)
+	hojas.append(hoja4)
+
+	if btn_izquierda:
+		btn_izquierda.connect("pressed", Callable(self, "_on_pagina_izquierda"))
+	if btn_derecha:
+		btn_derecha.connect("pressed", Callable(self, "_on_pagina_derecha"))
+
+	indice_actual = 0
+	_actualizar_hojas()
+
+
+func _on_pagina_derecha():
+	if indice_actual < hojas.size() - 1:
+		indice_actual += 1
+		_actualizar_hojas()
+		SoundManager.reproducir_efecto(4)
+
+
+func _on_pagina_izquierda():
+	if indice_actual > 0:
+		indice_actual -= 1
+		_actualizar_hojas()
+		SoundManager.reproducir_efecto(4)
+
+
+func _actualizar_hojas():
+	for i in range(hojas.size()):
+		hojas[i].visible = (i == indice_actual)
+
+	if btn_izquierda:
+		btn_izquierda.disabled = (indice_actual == 0)
+	if btn_derecha:
+		btn_derecha.disabled = (indice_actual == hojas.size() - 1)
